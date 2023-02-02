7787111f80cdbf15523eb3d9e0bf6a37950d3dd5
Prajwal-Adhikari <prazolppp@gmail.com>
2023-02-02 18:24:58 +0545

    llisted exchange features

---
+++ contracts/Exchange.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "hardhat/console.sol";
+
+/**
+ * @title Decentralized exchange
+ * @author Prawjal Adhikari
+ * @notice An exchange that lets you to swap tokens.
+ * @custom:todo 
+ * Deposit Tokens
+ * Withdraw Tokens
+ * Check Balances
+ * Make Orders
+ * Cancel Orders
+ * Fill Orders
+ * Charge Fees
+ * Track Fee Account
+ * 
+ */
+
+
+contract Exchange {
+
+}
