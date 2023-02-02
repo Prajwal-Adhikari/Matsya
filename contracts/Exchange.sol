0485a44b4b8dc85064d695f6974e1b62eb6a360c
Prajwal-Adhikari <prazolppp@gmail.com>
2023-02-02 21:24:46 +0545

    smoke test for exchange setup

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -20,5 +20,13 @@ import "hardhat/console.sol";
 
 
 contract Exchange {
+    address public feeAccount;
+    uint256 public feePercent;
+
+    constructor(address _feeAccount, uint256 _feePercent){
+        feeAccount = _feeAccount;
+        feePercent = _feePercent;
+
+    }
 
 }
