2dd2c6cbda84767de2086e675b3fb3cf0794b91a
Prajwal-Adhikari <prazolppp@gmail.com>
2023-02-03 21:36:14 +0545

    Deposit to exchange test

--- test/Exchange.js
+++ test/Exchange.js
@@ -6,16 +6,21 @@ const tokens = (n) => {
 }
 
 describe('Exchange', () => {
-  let exchange, deployer, feeAccount;
+  let exchange, deployer, feeAccount, token1;
   const feePercent = 10;
-
+  
   beforeEach(async () => {
-      
+    
+    const Exchange = await ethers.getContractFactory('Exchange');
+    const Token = await ethers.getContractFactory("Token");
+
+
+      token1 = await Token.deploy("CHILLAR","$CHILL",'1000000');
       accounts = await ethers.getSigners()
       deployer = accounts[0]
       feeAccount = accounts[1]
+      user1 = accounts[2];
 
-      const Exchange = await ethers.getContractFactory('Exchange')
       exchange = await Exchange.deploy(feeAccount.address,feePercent);
     })
 
@@ -35,7 +40,14 @@ describe('Exchange', () => {
   describe("Depositing token in the exchange", () => {
 
     beforeEach(async() => {
-        //Approve 
+      
+      let transaction, result;
+      let amount = tokens(10);
+      
+      //Approve 
+
+      transaction = await exchange.connect(user1).depositeToken(token1.address,amount);
+
         //Deposite token
     })
 
