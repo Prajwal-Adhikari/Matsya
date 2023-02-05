945ff49ddeb4be1bbf899822a2c125acc97515ef
Prajwal-Adhikari <prazolppp@gmail.com>
2023-02-05 18:30:28 +0545

    Order placement feature on progress

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -24,9 +24,21 @@ contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
     mapping(address => mapping(address => uint256)) public tokens;
+    // mapping(uint256 => _Order);
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(address token, address user, uint256 amount, uint256 balance);
+
+    struct _Order{
+        //Attributes of an order
+        uint256 id; //Uniquie identifier for an order in the blockchain
+        address user; //User who placed the order
+        address tokenGet; // Address of the token they receive
+        uint256 amountGet; // Amount of the token they receive
+        address tokenGive; // Address of the token they give
+        uint256 amountGive; // Amount of the token they give
+        uint256 timestamp; // Time when the order was placed
+    }
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -69,6 +81,25 @@ contract Exchange {
         returns (uint256)
     {
         return tokens[_token][_user];
+    }
+
+
+    //MAKE AND CANCEL ORDERS
+
+    /**  @dev Token Give : the token the trader wants to give; which token and how much
+     *        Token Get : the token the trader wants to get; which token and how much
+     
+*/
+    function placeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+        _Order(
+            1, //id
+            msg.sender, //user
+            _tokenGet, //tokenGet
+            _amountGet, //amountGet
+            _tokenGive, //tokenGive
+            _amountGive, //amountGive
+            block.timestamp //timestamp
+        )
     }
 
 }
